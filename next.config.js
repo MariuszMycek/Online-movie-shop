6de49095810dbb333916168a717577042e0e1e0e
Mariusz Mycek <m.mycek83@gmail.com>
2019-07-24 14:20:30 +0200

    Add offline suuport

--- next.config.js
+++ next.config.js
@@ -1,16 +1,40 @@
 const withSass = require('@zeit/next-sass');
+const withOffline = require('next-offline');
 
-module.exports = withSass({
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    (config.node = {
-      fs: 'empty',
-    }),
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
+module.exports = withOffline(
+  withSass({
+    webpack: config => {
+      // Fixes npm packages that depend on `fs` module
+      (config.node = {
+        fs: 'empty',
+      }),
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        });
 
-    return config;
-  },
-});
+      return config;
+    },
+    transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+    workboxOpts: {
+      swDest: 'service-worker.js',
+      runtimeCaching: [
+        {
+          urlPattern: /^https?.*/,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'https-calls',
+            networkTimeoutSeconds: 15,
+            expiration: {
+              maxEntries: 150,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+            },
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
+    },
+  })
+);
